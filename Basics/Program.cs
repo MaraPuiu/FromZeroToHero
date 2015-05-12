df5f29986f0d1dfca3d5caa3b26311b99db5cced
MaraPuiu <mara_puiu@yahoo.com>
2015-05-12 16:51:00 +0300

    Exercise 1

---
+++ Basics/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime t = DateTime.Now;
+            bool response = checkIfWeekend(t);
+            Console.WriteLine("Is weekend: " + response);
+        }
+
+        public static bool checkIfWeekend(DateTime date)
+        {
+            if (date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday) return true;
+            return false;
+        }
+    }
+}
