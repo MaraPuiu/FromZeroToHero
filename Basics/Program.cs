c88db6da7ba8768d03004946937f241c22d14c43
MaraPuiu <mara_puiu@yahoo.com>
2015-05-12 17:03:16 +0300

    Exercise 2

--- Basics/Program.cs
+++ Basics/Program.cs
@@ -8,17 +8,42 @@ namespace Basics
 {
     class Program
     {
+        public enum Seasons { Winter = 0, Spring = 1, Summer = 2, Autumn = 3}
+
         static void Main(string[] args)
         {
-            DateTime t = DateTime.Now;
-            bool response = checkIfWeekend(t);
-            Console.WriteLine("Is weekend: " + response);
+            //DateTime t = DateTime.Now;
+            //bool response = checkIfWeekend(t);
+            //Console.WriteLine("Is weekend: " + response);
+           
+            Seasons season = Seasons.Winter;
+            customSeasonMessage(season);
         }
 
         public static bool checkIfWeekend(DateTime date)
         {
             if (date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday) return true;
             return false;
+        }
+
+        public static void customSeasonMessage(Seasons enumSeason)
+        {
+            switch (enumSeason)
+            {
+                case Seasons.Autumn:
+                    Console.WriteLine("Hello Autumn");
+                    break;
+                case Seasons.Winter:
+                    Console.WriteLine("Hello Winter");
+                    break;
+                case Seasons.Summer:
+                    Console.WriteLine("Hello Summer");
+                    break;
+                case Seasons.Spring:
+                    Console.WriteLine("Hello Spring");
+                    break;
+            }
+
         }
     }
 }
