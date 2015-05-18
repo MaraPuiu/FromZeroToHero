1778fc675b8a73b91c79886edf0b8ff454cdb3bc
MaraPuiu <mara_puiu@yahoo.com>
2015-05-18 13:19:53 +0300

    MVC Lab1+2

---
+++ FzthMVC/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FzthMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        //[AcceptVerbs(HttpVerbs.Post)]
+        [HttpGet]
+        public string Hello(Person person)
+        {
+            return person.ToString();
+        }
+
+        public class Person
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+
+            public override string ToString()
+            {
+                return String.Format("Hello {0}, age {1}", Name, Age);
+            }
+        }
+    }
+}
---
+++ FzthMVC/Controllers/HotelController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FZTH.MVC.DataAccess;
+
+namespace FzthMVC.Controllers
+{
+    public class HotelController : Controller
+    {
+        //
+        // GET: /Hotel/
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Detail(Int32 id)
+        {
+            var hotel = Data.Hotels.FirstOrDefault(x => x.Id == id);
+            if (hotel != null)
+            {
+                return Json(hotel, JsonRequestBehavior.AllowGet);
+            }
+            return HttpNotFound();
+        }
+	}
+}
